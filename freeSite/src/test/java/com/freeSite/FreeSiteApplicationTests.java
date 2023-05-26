be6d20009dfe1d04f584dcae3a87810e3323d217
Deniz000 <guldenizozdemir03@gmail.com>
2023-05-27 01:19:47 +0300

    start project

---
+++ freeSite/src/test/java/com/freeSite/FreeSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
